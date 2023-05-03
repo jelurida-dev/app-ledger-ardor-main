0255e937ae414a9720c6c83666f25b63deeb64e4
Sergi Baila <sergibaila@protonmail.com>
2023-05-03 16:38:57 +0200

    move SDK code to new standard_app SDK

--- src/app_main.c
+++ src/app_main.c
@@ -3,6 +3,7 @@
 *  (c) 2016 Ledger
 *  (c) 2018 Nebulous
 *  (c) 2019 Haim Bender
+*  (c) 2021-2023 Jelurida IP B.V.
 *
 *
 *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -27,9 +28,6 @@
 #include "config.h"
 #include "ardor.h"
 #include "returnValues.h"
-
-ux_state_t G_ux;
-bolos_ux_params_t G_ux_params;	
 
 UX_STEP_NOCB(
     ux_idle_flow_1_step, 
@@ -135,7 +133,9 @@ void fillBufferWithAnswerAndEnding(const uint8_t answer, uint8_t * const tx) {
 // subsequent io_exchange call. The handler may also throw an exception, which
 // will be caught, converted to an error code, appended to the response APDU,
 // and sent in the next io_exchange call.
-static void ardor_main(void) {
+void app_main(void) {
+
+	ui_idle();
 
 	lastCmdNumber = 0;
 
@@ -212,160 +212,4 @@ static void ardor_main(void) {
 		}
 		END_TRY;
 	}
-}
-
-
-// Everything below this point is Ledger magic. And the magic isn't well-
-// documented, so if you want to understand it, you'll need to read the
-// source, which you can find in the nanos-secure-sdk repo. Fortunately, you
-// don't need to understand any of this in order to write an app.
-//
-// Next, we'll look at how the various commands are implemented. We'll start
-// with the sizeofmplest command, signHash.c.
-
-// override point, but nothing more to do
-void io_seproxyhal_display(const bagl_element_t *element) {
-	io_seproxyhal_display_default((bagl_element_t *)element);
-}
-
-unsigned char G_io_seproxyhal_spi_buffer[IO_SEPROXYHAL_BUFFER_SIZE_B];
-
-unsigned char io_event(unsigned char channel) {
-
-	UNUSED(channel);
-
-	// can't have more than one tag in the reply, not supported yet.
-	switch (G_io_seproxyhal_spi_buffer[0]) {
-	case SEPROXYHAL_TAG_FINGER_EVENT:
-		UX_FINGER_EVENT(G_io_seproxyhal_spi_buffer);
-		break;
-
-	case SEPROXYHAL_TAG_BUTTON_PUSH_EVENT:
-		UX_BUTTON_PUSH_EVENT(G_io_seproxyhal_spi_buffer);
-		break;
-
-#ifdef HAVE_BLE
-    // Make automatically discoverable again when disconnected
-    case SEPROXYHAL_TAG_BLE_CONNECTION_EVENT:
-        if (G_io_seproxyhal_spi_buffer[3] == 0) {
-            // TODO : cleaner reset sequence
-            // first disable BLE before turning it off
-            G_io_seproxyhal_spi_buffer[0] = SEPROXYHAL_TAG_BLE_RADIO_POWER;
-            G_io_seproxyhal_spi_buffer[1] = 0;
-            G_io_seproxyhal_spi_buffer[2] = 1;
-            G_io_seproxyhal_spi_buffer[3] = 0;
-            io_seproxyhal_spi_send(G_io_seproxyhal_spi_buffer, 4);
-            // send BLE power on (default parameters)
-            G_io_seproxyhal_spi_buffer[0] = SEPROXYHAL_TAG_BLE_RADIO_POWER;
-            G_io_seproxyhal_spi_buffer[1] = 0;
-            G_io_seproxyhal_spi_buffer[2] = 1;
-            G_io_seproxyhal_spi_buffer[3] = 3; // ble on & advertise
-            io_seproxyhal_spi_send(G_io_seproxyhal_spi_buffer, 5);
-        }
-        break;
-#endif
-        
-	case SEPROXYHAL_TAG_STATUS_EVENT:
-		if (G_io_apdu_media == IO_APDU_MEDIA_USB_HID &&
-			!(U4BE(G_io_seproxyhal_spi_buffer, 3) &
-			  SEPROXYHAL_TAG_STATUS_EVENT_FLAG_USB_POWERED)) {
-			THROW(EXCEPTION_IO_RESET);
-		}
-		UX_DEFAULT_EVENT();
-		break;
-
-	case SEPROXYHAL_TAG_DISPLAY_PROCESSED_EVENT:
-		UX_DISPLAYED_EVENT({});
-		break;
-
-	case SEPROXYHAL_TAG_TICKER_EVENT:
-		UX_TICKER_EVENT(G_io_seproxyhal_spi_buffer, {});
-		break;
-
-	default:
-		UX_DEFAULT_EVENT();
-		break;
-	}
-
-	// close the event if not done previously (by a display or whatever)
-	if (!io_seproxyhal_spi_is_status_sent()) {
-		io_seproxyhal_general_status();
-	}
-
-	// command has been processed, DO NOT reset the current APDU transport
-	return 1;
-}
-
-unsigned short io_exchange_al(unsigned char channel, unsigned short tx_len) {
-	switch (channel & ~(IO_FLAGS)) {
-	case CHANNEL_KEYBOARD:
-		break;
-	// multiplexed io exchange over a SPI channel and TLV encapsulated protocol
-	case CHANNEL_SPI:
-		if (tx_len) {
-			io_seproxyhal_spi_send(G_io_apdu_buffer, tx_len);
-			if (channel & IO_RESET_AFTER_REPLIED) {
-				reset();
-			}
-			return 0; // nothing received from the master so far (it's a tx transaction)
-		} else {
-			return io_seproxyhal_spi_recv(G_io_apdu_buffer, sizeof(G_io_apdu_buffer), 0);
-		}
-	default:
-		THROW(INVALID_PARAMETER);
-	}
-	return 0;
-}
-
-static void app_exit(void) {
-	BEGIN_TRY_L(exit) {
-		TRY_L(exit) {
-			os_sched_exit(-1);
-		}
-		FINALLY_L(exit) {
-		}
-	}
-	END_TRY_L(exit);
-}
-
-__attribute__((section(".boot"))) int main(void) {
-	// exit critical section
-	__asm volatile("cpsie i");
-
-	for (;;) {
-		UX_INIT();
-		os_boot();
-		BEGIN_TRY {
-			TRY {
-				io_seproxyhal_init();
-				#ifdef TARGET_NANOX
-            		// grab the current plane mode setting
-            		G_io_app.plane_mode = os_setting_get(OS_SETTING_PLANEMODE, NULL, 0);
-				#endif // TARGET_NANOX
-				USB_power(0);
-				USB_power(1);
-				ui_idle();
-
-				#ifdef HAVE_BLE
-            		BLE_power(0, NULL);
-            		BLE_power(1, "Nano X");
-				#endif // HAVE_BLE
-
-            	ardor_main();
-
-			}
-			CATCH(EXCEPTION_IO_RESET) {
-				// reset IO and UX before continuing
-				continue;
-			}
-			CATCH_ALL {
-				break;
-			}
-			FINALLY {
-			}
-		}
-		END_TRY;
-	}
-	app_exit();
-	return 0;
 }
