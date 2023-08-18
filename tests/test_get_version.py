9a3107a4bf41364a544640e841add090b5090cc0
Sergi Baila <sergibaila@protonmail.com>
2023-08-18 12:48:05 +0200

    bump version to 1.1.0

--- tests/test_get_version.py
+++ tests/test_get_version.py
@@ -5,4 +5,4 @@ INS_GET_VERSION = 0x01
 def test_get_version(backend):
     rapdu = backend.exchange(cla=CLA, ins=INS_GET_VERSION)
     assert rapdu.status == RESPONSE_SUFFIX
-    assert rapdu.data.hex() == "01000400babe00"
+    assert rapdu.data.hex() == "01010000babe00"
