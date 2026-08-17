566f24247932d1056df4d8df166288ea7d88836b
Sergi Baila <sergibaila@protonmail.com>
2026-08-17 08:06:26 +0200

    Remove leftover Nano S-specific flow logic

--- src/ui/blind_sign_bagl.c
+++ src/ui/blind_sign_bagl.c
@@ -3,29 +3,13 @@
 #include "blind_sign_bagl.h"
 #include "ux.h"
 
-callback_void_t bsNotEnabledCallback;
-
-static void blindSigningNotEnabledCallback() {
-    bsNotEnabledCallback();
-}
-
-#ifdef TARGET_NANOS
-
-UX_STEP_CB(ux_step_blind_signing_not_enabled,
-           bnnn_paging,
-           blindSigningNotEnabledCallback(),
-           {"ERROR", "Blind signing must be enabled on Settings"});
-UX_FLOW(ux_flow_blind_signing_not_enabled, &ux_step_blind_signing_not_enabled);
-
-#else
+static callback_void_t bsNotEnabledCallback;
 
 UX_STEP_CB(ux_step_blind_signing_not_enabled,
            pnn,
-           blindSigningNotEnabledCallback(),
+           bsNotEnabledCallback(),
            {&C_icon_crossmark, "Blind signing must be", "enabled on Settings"});
 UX_FLOW(ux_flow_blind_signing_not_enabled, &ux_step_blind_signing_not_enabled);
-
-#endif
 
 void blindSigningNotEnabledScreen(callback_void_t callback) {
     bsNotEnabledCallback = callback;
